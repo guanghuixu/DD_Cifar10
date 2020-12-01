5622516763e8b2ff249f050ff9b2ef5045099fc7
wzq <957918462@qq.com>
2020-12-01 10:24:10 +0800

    first commit, debug our_dataloader

--- main_split.py
+++ main_split.py
@@ -13,6 +13,20 @@ import argparse
 
 from models import *
 from utils import progress_bar
+import random
+import numpy as np
+
+def seed_torch(seed):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed) 
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed) 
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    
+seed_torch(2021)
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
@@ -39,15 +53,34 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
+def transform_tensor(dataset):
+    tensors = []
+    labels = []
+    for (inputs, targets) in dataset:
+        tensors.append(inputs)
+        labels.append(targets)
+    tensors = torch.stack(tensors)
+    labels = torch.LongTensor(labels)
+    return (tensors, labels)
+
+def shuffle_dataset(dataset):
+    tensors, labels = dataset
+    dataset_size = labels.size(0)
+    idx = random.sample(np.arange(dataset_size).tolist(), dataset_size)
+    return (tensors[idx], labels[idx])
+
+
 trainset = torchvision.datasets.CIFAR10(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
+our_trainloader = transform_tensor(trainset)
 
 testset = torchvision.datasets.CIFAR10(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
+our_testloader = transform_tensor(testset)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
@@ -90,13 +123,17 @@ scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
 
 
 # Training
-def train(epoch):
+def train(epoch, our_trainloader):
     print('\nEpoch: %d' % epoch)
     net.train()
     train_loss = 0
     correct = 0
     total = 0
-    for batch_idx, (inputs, targets) in enumerate(trainloader):
+    # for batch_idx, (inputs, targets) in enumerate(trainloader):
+    our_trainloader = shuffle_dataset(our_trainloader)
+    for batch_idx in range(0, len(trainset)//128+1, 1):
+        inputs = our_trainloader[0][batch_idx*128: (batch_idx+1)*128]
+        targets = our_trainloader[1][batch_idx*128: (batch_idx+1)*128]
         inputs, targets = inputs.to(device), targets.to(device)
         optimizer.zero_grad()
         outputs = net(inputs)
@@ -120,7 +157,10 @@ def test(epoch):
     correct = 0
     total = 0
     with torch.no_grad():
-        for batch_idx, (inputs, targets) in enumerate(testloader):
+        # for batch_idx, (inputs, targets) in enumerate(testloader):
+        for batch_idx in range(0, len(testset)//100, 1):
+            inputs = our_testloader[0][batch_idx*100: (batch_idx+1)*100]
+            targets = our_testloader[1][batch_idx*100: (batch_idx+1)*100]
             inputs, targets = inputs.to(device), targets.to(device)
             outputs = net(inputs)
             loss = criterion(outputs, targets)
@@ -149,6 +189,6 @@ def test(epoch):
 
 
 for epoch in range(start_epoch, start_epoch+200):
-    train(epoch)
+    train(epoch, our_trainloader)
     test(epoch)
     scheduler.step()
